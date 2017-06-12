4e272d750fc65268cc5edd3722685eeeb21b4d4c
Parker Stevens <pstevens33@gmail.com>
2017-06-12 13:34:53 -0600

    Running 300 mse sigmoid adam 0.50

--- src/cnn2.py
+++ src/cnn2.py
@@ -89,7 +89,7 @@ model.add(Dense(input_dim=num_neurons_in_layer,
                  init='uniform',
                  activation='sigmoid')) # only 12 neurons - keep softmax at last layer
 sgd = SGD(lr=0.001, decay=1e-7, momentum=0.95) # using stochastic gradient descent (keep)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'] ) # (keep)
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'] ) # (keep)
 
 
 # batch_size = 16
@@ -233,7 +233,7 @@ plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.savefig('../data/plots/acc_300_binary_sigmoid_adam_0.50.png')
+plt.savefig('../data/plots/acc_300_mse_sigmoid_adam_0.50.png')
 plt.close()
 
 plt.plot(history.history['loss'])
@@ -242,7 +242,7 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.savefig('../data/plots/loss_300_binary_sigmoid_adam_0.50.png')
+plt.savefig('../data/plots/loss_300_mse_sigmoid_adam_0.50.png')
 plt.close()
 
-model.save('../data/models/300_binary_sigmoid_adam_0.50.h5')
+model.save('../data/models/300_mse_sigmoid_adam_0.50.h5')
