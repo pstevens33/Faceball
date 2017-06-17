c42cc73a5ae52a062dcec7bd893cbeecdfe87f84
Parker Stevens <pstevens33@gmail.com>
2017-06-16 18:45:18 -0600

    Changing production port

--- flask_app/app_production.py
+++ flask_app/app_production.py
@@ -147,4 +147,4 @@ def technical():
 
 if __name__ == '__main__':
     model = load_model('models/gpu_300_players_sigmoid_binary.h5')
-    app.run(host='0.0.0.0', port=8105, debug=False)
+    app.run(host='ec2-34-226-24-213.compute-1.amazonaws.com', port=8105, debug=False)
