06bfb3c88178f755cbe20186a3bc7495971a3b38
Ubuntu <ubuntu@ip-172-31-78-59.ec2.internal>
2017-06-17 05:58:13 +0000

    Altered setup.py

---
+++ flask_app/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='flask_app',
+    version='1.0',
+    long_description=__doc__,
+    packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['openface']
+)
