863774616d59232e23472b9535708db8d0627a32
Parker Stevens <pstevens33@gmail.com>
2017-07-05 15:55:32 -0600

    Updated cnn_new.py

--- src/cnn_new.py
+++ src/cnn_new.py
@@ -14,42 +14,30 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import History 
-# import theano
 from sklearn.cross_validation import train_test_split
-# import os
-
-# os.environ["THEANO_FLAGS"] = "device=cuda, assert_no_cpu_op=True"
 
 history = History()
 
+# Load data from saved numpy arrays
 X = np.load('../data/X_players.npy')
 y = np.load('../data/y_players.npy')
 
-# X = X[:500]
-# y = y[:500]
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=43)
+# One hot encoding for multi-class labels
 y_ohe = np_utils.to_categorical(y)
 
+# Original Shape of Images
 input_shape = (128, 128, 3)
 
-
 ''' defines multi-layer-perceptron neural network '''
-# available activation functions at:
-# https://keras.io/activations/
-# https://en.wikipedia.org/wiki/Activation_function
-# options: 'linear', 'sigmoid', 'tanh', 'relu', 'softplus', 'softsign'
-# there are other ways to initialize the weights besides 'uniform', too
-
 model = Sequential() # sequence of layers
 num_neurons_in_layer = 64 # number of neurons in a layer
 num_inputs = X.shape[1] # number of features (784)
 num_classes = y_ohe.shape[1]  # number of classes, 0-9
 
+# 5 Convolution Layers (Dropout of 0.35 reduced overfitting and still allowed for adequate learning)
 model.add(Conv2D(32, 3, 3, input_shape=input_shape, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.20))
+model.add(Dropout(0.35))
 
 model.add(Conv2D(32, 3, 3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -67,34 +55,28 @@ model.add(Conv2D(128, 3, 3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.35))
 
-
+# Flatten to make proper dimensions
 model.add(Flatten())
 
+# 3 Fully-Connected layers
 model.add(Dense(input_dim=128,
                  output_dim=num_neurons_in_layer,
                  init='uniform',
                  activation='relu'))
-# model.add(Dropout(0.5))
 model.add(Dense(input_dim=num_neurons_in_layer,
                  output_dim=num_neurons_in_layer,
                  init='uniform',
                  activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dropout(0.5))
 model.add(Dense(input_dim=num_neurons_in_layer,
                  output_dim=num_classes,
                  init='uniform',
-                 activation='sigmoid')) # only 12 neurons - keep softmax at last layer
-sgd = SGD(lr=0.001, decay=1e-7, momentum=0.95) # using stochastic gradient descent (keep)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'] ) # (keep)
+                 activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Necessary in order to plot loss and accuracy for each epoch
+history = model.fit(X, y_ohe, epochs=300, batch_size=128, shuffle=True, verbose=1, validation_split=0.25) # 
 
-unique, counts = np.unique(y, return_counts=True)
-class_count_dict = dict(zip(unique, counts))
-
-history = model.fit(X, y_ohe, epochs=300, batch_size=128, shuffle=True, verbose=1, validation_split=0.25) # cross val to estimate test error
-
-
+# Plot history and loss during training
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('model accuracy')
